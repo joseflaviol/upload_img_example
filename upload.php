840c9c68aae5aeea6752e8ba6adbd92ad318264a
José Flávio <joseflaviol119@gmail.com>
2018-09-16 00:58:35 -0300

    Third Commit

--- upload.php
+++ upload.php
@@ -8,5 +8,10 @@
   }
   $caminho = "imagens/imagem.$ext";//define onde a imagem vai ser salva (neste exemplo, seria esta string que ficaria salva no banco)
 
-  move_uploaded_file($caminho_temporario, $caminho);//faz upload da imagem
+  if(move_uploaded_file($caminho_temporario, $caminho)){//faz upload da imagem
+    echo "Imagem foi upada com sucesso."
+  }
+  else{
+    echo "Falha ao upar imagem."
+  }
 ?>
